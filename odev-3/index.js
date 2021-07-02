3ced5b99b54c4c2077110ee87a47d8a6a008ac28
Othmane Agrram <oagrram@e2r10p4.1337.ma>
2021-07-02 20:15:10 +0100

    finish

--- odev-3/index.js
+++ odev-3/index.js
@@ -1,34 +1,47 @@
 function checkForm() {
 
-    var userNameCheck = document.getElementById("username");
-    var passwordCheck = document.getElementById("password");
-    var emailCheck = document.getElementById("email");
-    var checkboxcheck = document.getElementById("checkbox");
+    var userName = document.getElementById("username");
+    var password = document.getElementById("password");
+    var email = document.getElementById("email");
+    var checkbox = document.getElementById("checkbox");
     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     var is_valid = false;
+
+    // Hide all errors at starting
 
     hide_errors("usname_eror_msg", "pass_eror_msg", "pass_length_eror", "email_eror_msg", "email_valid_eror", "checkbox_eror_msg");
 
     is_valid = true;
-    if (userNameCheck.value == "") {
+
+    // check username input value
+    if (userName.value == "") {
         set_eror_and_display("usname_eror_msg", "Lutfen Bu Alani doldurun");
         is_valid = false;
     }
-    if (passwordCheck.value == "") {
+
+    // check password input value and lenght
+
+    if (password.value == "") {
         set_eror_and_display("pass_eror_msg", "Lutfen Bu Alani doldurun");
         is_valid = false;
-    } else if (passwordCheck.value.length < 8) {
+    } else if (password.value.length < 8) {
         set_eror_and_display("pass_length_eror", "şifre en az 8 karakter uzunluğunda olmalıdır");
         is_valid = false;
     }
-    if (emailCheck.value == "") {
+
+    // check email input value and validation
+
+    if (email.value == "") {
         set_eror_and_display("email_eror_msg", "Lutfen Bu Alani doldurun");
         is_valid = false;
-    } else if (!emailPattern.test(emailCheck.value)) {
+    } else if (!emailPattern.test(email.value)) {
         set_eror_and_display("email_valid_eror", "Geçerli bir");
         is_valid = false;
     }
-    if (checkboxcheck.checked == false) {
+
+    // checkbox 
+
+    if (checkbox.checked == false) {
         set_eror_and_display("checkbox_eror_msg", "Lutfen Bu Alani doldurun");
         is_valid = false;
     }
